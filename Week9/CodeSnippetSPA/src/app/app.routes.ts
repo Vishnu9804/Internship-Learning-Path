aa88261781de88e6e674fa43803f6500043d2f83
Vishnu9804 <burkhawalavishnu@gmail.com>
2026-03-17 20:54:38 +0530

    Added Working Routes and Added Navbar

--- Week9/CodeSnippetSPA/src/app/app.routes.ts
+++ Week9/CodeSnippetSPA/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './Components/login/login';
+import { Signup } from './Components/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: Login},
+    {path: "signup", component:Signup}
+];
