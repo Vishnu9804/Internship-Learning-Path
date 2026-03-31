a835723bd8ee444c5bc00b228c962b9d7b6549f5
Vishnu9804 <burkhawalavishnu@gmail.com>
2026-03-31 20:27:13 +0530

    create base layout with header and accessible theme toggle

--- Week11/BlogUI/src/app/components/header/header.component.ts
+++ Week11/BlogUI/src/app/components/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { AsyncPipe } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule, AsyncPipe], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public themeService: ThemeService) {}
+
+  toggle() {
+    this.themeService.toggleTheme();
+  }
+}
