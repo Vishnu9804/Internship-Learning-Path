9e7fc663ea15f71d9add65cc29fa3dd52578f466
Vishnu9804 <burkhawalavishnu@gmail.com>
2026-03-31 19:03:13 +0530

    Initiated Week 11

---
+++ Week11/BlogUI/src/app/components/blog-details/blog-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-details',
+  imports: [],
+  templateUrl: './blog-details.component.html',
+  styleUrl: './blog-details.component.css',
+})
+export class BlogDetailsComponent {}
